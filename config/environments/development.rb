d7f1420ba98419f21ac2a32f8125142474093ccd
vinylhousegarage <vinylhousegarage@gmail.com>
2024-07-24 20:11:45 +0900

    modify

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "sample-dxth.onrender.com"
+  config.web_console.whitelisted_ips = [ "172.16.0.0/12" ]
+   
 end
