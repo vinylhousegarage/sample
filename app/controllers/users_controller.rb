3e55466108a57251403b72e3ec3b5a2986c55549
vinylhousegarage <vinylhousegarage@gmail.com>
2024-08-09 10:34:23 +0900

    generate users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  # 合算集計表示
+  def index
+  end
+
+  # 個人集計表示
+  def show
+  end
+
+  # 招待用QRコード表示
+  def login_form
+  end
+
+  # ログイン
+  def login
+  end
+
+  # ログアウト
+  def logout
+  end
+end
